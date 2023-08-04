4c1d3cc146e85449e7b72c90210004b78ed59182
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-04 15:47:31 -0600

    test: #all_countries

--- spec/services/countries_service_spec.rb
+++ spec/services/countries_service_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Countries Service' do
-  before(:each) do
-    
-  end
+  describe 'class methods', :vcr do 
+    it '#all_countries' do 
+      response = CountriesService.all_countries
 
-  describe 'relationships' do
+      expect(response).to be_a Array
+      
+      response.each do |r|
+        expect(r).to have_key(:name)
+        expect(r[:name]).to be_a Hash
+        expect(r[:name]).to have_key(:common)
+        expect(r[:name][:common]).to be_a String
+      end
+    end
   end
 end

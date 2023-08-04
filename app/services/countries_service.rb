9d112210e1e16ca2f4da2a9fc7ad3e78250d6651
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-04 16:04:24 -0600

    feat: all_countries

---
+++ app/services/countries_service.rb
@@ -0,0 +1,17 @@
+class CountriesService
+  class << self 
+    def all_countries
+      parse_response(conn.get("all"))
+    end
+    
+    private 
+    
+    def conn 
+      Faraday.new("https://restcountries.com/v3.1/")
+    end
+    
+    def parse_response(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end

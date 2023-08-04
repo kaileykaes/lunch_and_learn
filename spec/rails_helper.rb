30e08585d4f42ec77a1df24b4f8aab777f422d29
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-04 15:41:37 -0600

    configure vcr and webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -69,4 +70,11 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<TMDB_AUTH>') { ENV["TMDB_AUTH"] }
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
 end
---
+++ spec/services/countries_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Countries Service' do
+  before(:each) do
+    
+  end
+
+  describe 'relationships' do
+  end
+end

0a3dd6e84279064ed02ff04317c7d238b51d194c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-06 15:10:47 -0600

    create recipes search endpoint

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::RecipesController < ApplicationController
 
   def index
+    unless params[:country].present?
+      params[:country] = CountriesFacade.new.random_country_name
+    end
     response = recipes_facade.recipes_search(params[:country])
     render json: RecipeSerializer.new(response)
   end
--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -1,17 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes Requests' do
-  before(:each) do
-    get "/api/v1/recipes?country=laos" 
-  end
-
   describe 'endpoint' do
+    # before(:each) do
+    # end
+    
     it 'hits the endpoint' do 
+      get "/api/v1/recipes?country=laos" 
       expect(response).to be_successful
     end
-
+    
     it 'returns serialized json' do 
+      get "/api/v1/recipes?country=laos" 
       recipes = JSON.parse(response.body, symbolize_names: true)[:data]
+      recipes.each do |recipe|
+        check_hash_structure(recipe, :attributes, Hash)
+        check_hash_structure(recipe, :type, String)
+        check_hash_structure(recipe[:attributes], :title, String)
+        check_hash_structure(recipe[:attributes], :url, String)
+        check_hash_structure(recipe[:attributes], :image, String)
+        check_hash_structure(recipe[:attributes], :country, String)
+        expect(recipe[:attributes][:country]).to eq('Laos')
+        expect(recipe).to_not have_key(:source)
+        expect(recipe).to_not have_key(:uri)
+      end
+    end
+  end
+
+  describe 'sad paths' do 
+    it 'if no country given, returns recipes from random country' do 
+      get '/api/v1/recipes' 
+
+      expect(response).to be_successful
+      
+      recipes = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(recipes).to be_a Array
+      expect(recipes).to_not be_empty
+
       recipes.each do |recipe|
         check_hash_structure(recipe, :attributes, Hash)
         check_hash_structure(recipe, :type, String)
@@ -21,7 +47,16 @@ RSpec.describe 'Recipes Requests' do
         check_hash_structure(recipe[:attributes], :country, String)
         expect(recipe).to_not have_key(:source)
         expect(recipe).to_not have_key(:uri)
+        expect(recipe[:attributes][:title]).to_not be_empty
       end
+    end
+
+    it 'if nothing returned, response is an empty data array' do
+      get '/api/v1/recipes?country=sudan'
+      
+      expect(response).to be_successful
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result).to eq({ data: [] })
     end
   end
 end

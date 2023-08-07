5352e8718596cfd795decb6ad8775bc4d86d99dc
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 10:14:23 -0600

    create json from air quality object similar to example

---
+++ spec/serializers/air_quality_serializer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Air Quality Serializer' do
+  describe 'serialization' do
+    it 'serializes a single air quality object' do 
+      air_quality = AirQualityFacade.new.air_quality_by_country('Nigeria')
+
+      serialized_air_quality = AirQualitySerializer.new(air_quality).serializable_hash.to_json
+      expect(serialized_air_quality).to be_a String
+
+      parsed_air_quality = JSON.parse(serialized_air_quality, symbolize_names: true)
+
+      expect(parsed_air_quality).to be_a Hash
+      check_hash_structure(parsed_air_quality, :data, Hash)
+      expect(parsed_air_quality[:data][:id]).to eq(nil.to_json)
+      check_hash_structure(parsed_air_quality[:data], :type, String)
+      check_hash_structure(parsed_air_quality[:data], :attributes, Hash)
+
+      attributes = parsed_air_quality[:data][:attributes]
+
+      check_hash_structure(attributes, :aqi, Integer)
+      check_hash_structure(attributes, :pm25_concentration, Float)
+      check_hash_structure(attributes, :co_concentration, Float)
+      check_hash_structure(attributes, :city, String)
+    end
+  end
+end

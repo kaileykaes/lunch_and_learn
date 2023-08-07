ec082a4b1be88a30d289f57a1881805c3e25cdef
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 14:36:51 -0600

    test: learning resource serialization

---
+++ spec/serializers/learning_resource_serializer_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Learning Resource Serializer' do
+  before(:each), :vcr do
+    @video = VideosFacade.new.video_search('peru').sample
+    @images = ImagesFacade.new.images_search('peru')
+  end
+
+  describe 'serialization' do
+    it 'serializes learning resources' do 
+      serialized_resource = LearningResourceSerializer.format_resources(@video, @images, 'peru')
+
+      resource = JSON.parse(serialized_resource, symbolize_names: true)
+
+      check_hash_structure(resource, :data, Hash)
+
+      resource_data = resource[:data]
+      
+      expect(resource_data).to be_a Hash
+      check_hash_structure(resource_data, :id, String)
+      expect(resource_data[:id]).to eq(nil.to_json)
+      check_hash_structure(resource_data, :type, String)
+      expect(resource_data[:type]).to eq('learning_resource')
+      check_hash_structure(resource_data, :attributes, Hash)\
+
+      resource_attributes = resource_data[:attributes]
+
+      check_hash_structure(resource_attributes, :country, String)
+      check_hash_structure(resource_attributes, :video, Hash)
+      check_hash_structure(resource_attributes, :images, Array)
+      
+      video = resource_attributes[:video]
+
+      check_hash_structure(video, :title, String)
+      check_hash_structure(video, :youtube_video_id, String)
+      
+      images = resource_attributes[:images]
+
+      images.each do |image|
+        check_hash_structure(image, :alt_tag, String)
+        check_hash_structure(image, :url, String)
+      end
+    end
+  end
+end

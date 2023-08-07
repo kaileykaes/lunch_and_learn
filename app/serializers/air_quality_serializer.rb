b5ef6fe7475df450fd88f895b16be8a50c263c3a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 10:50:47 -0600

    refactor air quality serialization

--- app/serializers/air_quality_serializer.rb
+++ app/serializers/air_quality_serializer.rb
@@ -1,4 +1,20 @@
 class AirQualitySerializer
   include JSONAPI::Serializer
   attributes :aqi, :pm25_concentration, :co_concentration, :city
+
+  def self.format_air_quality(air_quality)
+    { data:
+      {   id: air_quality.id,
+          type: "air_quality",
+          city: air_quality.city,
+          attributes: 
+            { aqi: air_quality.aqi,
+              pm25_concentration: air_quality.pm25_concentration,
+              co_concentration: air_quality.co_concentration
+            } 
+      }
+    }.to_json
+  end
 end
+
+

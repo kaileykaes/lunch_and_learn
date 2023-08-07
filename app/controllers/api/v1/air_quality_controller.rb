a32b7f87ed600d2c76538851234f008ee3f4aeca
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 10:41:25 -0600

    air quality search by random country

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
-    air_quality = AirQualityFacade.new.air_quality_by_country('Nigeria')
-    render json: AirQualitySerializer.new(air_quality).serializable_hash.to_json
+    unless params[:country].present? 
+      params[:country] = CountriesFacade.new.random_country_name
+    end
+      air_quality = AirQualityFacade.new.air_quality_by_country(params[:country])
+      render json: AirQualitySerializer.new(air_quality).serializable_hash.to_json
   end
 end

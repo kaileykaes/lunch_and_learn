2b8b20751d789816c0db7e5d30dea87cb7c0c71d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 20:14:24 -0600

    test: unmatched password handling

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -35,7 +35,9 @@ RSpec.describe 'User Registration', type: :request do
       expect(user[:data][:attributes][:name]).to eq('George')
       expect(user[:data][:attributes][:email]).to eq('george@mosquito.org')
     end
+  end
 
+  describe 'sad paths' do 
     it 'throws error if email isnt unique' do 
       create(:user, email: 'george@mosquito.org')
       user_count = User.count
@@ -55,6 +57,33 @@ RSpec.describe 'User Registration', type: :request do
       error_attributes = error[:error]
       expect(error_attributes[:status]).to eq(403)
       expect(error_attributes[:message]).to eq('Validation failed: Email has already been taken')
+    end
+
+    it 'throws error if passwords dont match' do 
+      user_params = {
+        name: "George",
+        email: 'george@mosquito.org',
+        password: 'test',
+        password_confirmation: 'check'
+      }
+
+      user_count = User.count
+
+      post '/api/v1/users', headers: @headers, params: user_params, as: :json
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(403)
+      expect(user_count).to eq(user_count)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      check_hash_structure(error, :error, Hash)
+      check_hash_structure(error[:error], :status, Integer)
+      check_hash_structure(error[:error], :message, String)
+
+      error_attributes = error[:error]
+      expect(error_attributes[:status]).to eq(403)
+      expect(error_attributes[:message]).to eq("Validation failed: Password confirmation doesn't match Password, Password confirmation doesn't match Password")
     end
   end
 end

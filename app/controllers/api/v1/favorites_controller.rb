cd387c083ece43d5318428fc17671f2ee0e340f5
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-08 13:44:31 -0600

    favorites creation returns 201 status code

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key]) 
     if user != nil 
+      response.status = 201
       render json: success_message
     else
       authentication_fail

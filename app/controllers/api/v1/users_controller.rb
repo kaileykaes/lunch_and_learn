7253b58310a66a9e2273d2efb5b9a8f1be736aff
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 20:00:37 -0600

    test & feat: error serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.create!(user_params)
-    require 'pry'; binding.pry
     response.status = 201
     render json: UserSerializer.new(user).serializable_hash.to_json
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_response
+
+  def invalid_record_response(error)
+    require 'pry'; binding.pry
+
+    render json: ErrorSerializer.new(error).serialize_json, status: 403
+  end
 end

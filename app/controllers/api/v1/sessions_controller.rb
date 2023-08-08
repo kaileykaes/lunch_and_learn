96c8049a9905f0d19a3a14e99b129c2a21e6d250
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-07 21:22:35 -0600

    fix sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    if found_user != nil && found_user.authenticate(params[:password])
+    user = User.find_by(email: params[:email]) 
+    if user != nil && user.authenticate(params[:password])
       render json: UserSerializer.new(user)
     else
       authentication_fail
     end
-  end
-
-  private 
-  def found_user
-    User.find_by(email: params[:email]) 
   end
 end

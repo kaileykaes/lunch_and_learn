9af2e5bcc2bcdd05de785b865d01d3f31396bf73
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-08 21:18:02 -0600

    fix favorites creation

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::FavoritesController < ApplicationController
 
   def create
     if @user != nil 
+      @user.favorites.create!(favorite_params)
       response.status = 201
       render json: success_message
     else
@@ -19,6 +20,10 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   private
+  def favorite_params
+    params.permit(:country, :recipe_link, :recipe_title)
+  end
+
   def success_message
     {
       success: "Favorite added successfully."
@@ -26,6 +31,6 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   def find_user
-    @user = User.find_by(api_key: params[:api_key]) 
+    @user = User.find_by(api_key: params[:api_key])
   end
 end
